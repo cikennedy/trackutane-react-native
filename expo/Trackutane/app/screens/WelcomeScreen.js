94c9ba47ac1b847b273f72c2e64ae1334d108f9f
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-22 12:58:52 -0500

    adding screen directory and files

---
+++ expo/Trackutane/app/screens/WelcomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ImageBackground, StyleSheet } from "react-native";
+
+function WelcomeScreen(props) {
+  return;
+  <ImageBackground
+    source={require("../assets/Trackutane.png")}
+  ></ImageBackground>;
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+});
+
+export default WelcomeScreen;
