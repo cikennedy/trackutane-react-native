f749bcc079b7672b5dcf70d116364cf59b014884
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-29 12:45:30 -0500

    view image screen

---
+++ expo/Trackutane/app/screens/ViewImageScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+
+function ViewImageScreen(props) {
+  return (
+    <View style={styles.container}>
+      <Image resizeMode="contain" style={styles.image} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ViewImageScreen;
